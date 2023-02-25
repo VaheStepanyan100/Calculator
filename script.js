a4c24f11553ffe013fcdf2dc9c13cc3c98ce9a2a
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-02-25 21:26:37 +0400

    Add data-attributes for index.html and select elements in script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
